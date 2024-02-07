98499aacd43c94702c7ecc0775fcee6ee02045c0
Sahil Rajesh Shriwardhankar <102847495+notsointresting@users.noreply.github.com>
2024-02-07 08:12:35 +0000

    removed old code

--- Project-Management/Attendance/Log.py
+++ Project-Management/Attendance/Log.py
@@ -1,12 +1,12 @@
 import os
 import random
 
-for i in range(2):
-    d = str(i) + 'days ago'
-    rand = 27 # Generate a random day between 1 and 28
+for i in range(10):
+    d = 'just checking that is everything is fine or not'
+    rand = i + 1 # Generate a random day between 1 and 28
     with open('test.txt', 'a') as file:
         file.write(d + '\n')
     os.system('git add test.txt')
-    os.system('git commit --date="2023-01-' + str(rand) + ' ' + d + '" -m "Commit message"')
+    os.system('git commit --date="2024-01-' + str(rand) + ' ' + d + '" -m "Commit message"')
 
 os.system('git push -u origin main')
